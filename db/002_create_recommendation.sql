8459d9a2855fb0f88c02b2f3c54acdd33d37c54c
ahyolo27 <ahhyeon27@gmail.com>
2026-03-03 21:46:30 +0900

    chore: recommendation 테이블에 name 컬럼 추가

--- db/002_create_recommendation.sql
+++ db/002_create_recommendation.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS recommendation
 (
     uid        SERIAL PRIMARY KEY,
     problem_id INT NOT NULL,
+    name       TEXT NOT NULL,
     tag        JSON,
     created_at TIMESTAMP DEFAULT NOW()
 );
